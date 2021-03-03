44dcb62da4770ee9896735172d46786c72422bfa
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-03-03 21:06:14 +0000

    Servlet Mapping

---
+++ day01_hello/src/main/java/com/example/servlet/ErrorServlet.java
@@ -0,0 +1,33 @@
+package com.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @ClassName ErrorServlet
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/3/3 20:59
+ * @Version 1.0
+ */
+public class ErrorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>404</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+}
